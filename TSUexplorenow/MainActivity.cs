054daab57b33337579d3e4c6938a0b87f40e4517
Jonathan Kennås <jk95@live.se>
2018-04-23 10:51:27 +0200

    test

--- TSUexplorenow/MainActivity.cs
+++ TSUexplorenow/MainActivity.cs
@@ -11,7 +11,7 @@ namespace TSUexplorenow
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            // Set our view from the "main" layout resource 1
             SetContentView(Resource.Layout.Main);
         }
     }
